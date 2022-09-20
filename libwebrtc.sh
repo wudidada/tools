3dc014fe0f1d2a13b2c04e79a4a2a4bb114370d9
wudidada <hackerjiang@gmail.com>
2022-09-20 15:52:56 +0800

    fix del test case

--- libwebrtc.sh
+++ libwebrtc.sh
@@ -10,6 +10,7 @@ else
     out_dir="$1/libwebrtc"
 fi
 
+# copy .java
 java_dir=$out_dir/java/org/webrtc
 mkdir -p $java_dir
 
@@ -19,8 +20,12 @@ cp -Rv ${WEB_RTC_BASE}/rtc_base/**/org/webrtc/* ${java_dir}
 cp -Rv ${WEB_RTC_BASE}/modules/**/org/webrtc/* ${java_dir}
 cp -Rv ${NINJA_DIR}/gen/**/org/webrtc/* ${java_dir}
 
+rm -rf ${java_dir}/**/example
+rm -rf ${java_dir}/**/native_test
+
+# copy .so
 jni_dir=$out_dir/jni
-mkdir -p $java_dir
+mkdir -p $jni_dir
 
 cp -v $NINJA_DIR/libjingle_peerconnection_so.so $jni_dir
 

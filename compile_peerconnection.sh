24e95050e45e59046cda0f402141a32c656d0a20
wudidada <hackerjiang@gmail.com>
2022-09-22 19:05:53 +0800

    add compile peerconne script

---
+++ compile_peerconnection.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ "${RTC_BASE}" ]
+then
+    echo "RTC_BASE not set"
+    exit 1
+fi
+
+cd $RTC_BASE || exit 1
+gn gen out/arm64_new --args='target_os="android" target_cpu="arm64"'
+ninja -C out/arm64_new libjingle_peerconnection_so

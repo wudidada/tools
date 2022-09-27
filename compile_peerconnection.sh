f164db99c242e92cf7f99a1e8b62cadc7a851f42
wudidada <hackerjiang@gmail.com>
2022-09-27 09:59:35 +0800

    compile arm32 vervison

--- compile_peerconnection.sh
+++ compile_peerconnection.sh
@@ -2,6 +2,8 @@
 
 ARM64_OUT="out/arm64"
 ARM64_RELEASE="out/arm64_release"
+ARM32_OUT="out/arm32"
+ARM32_RELEASE="out/arm32_release"
 
 if [ -z "${RTC_BASE}" ]
 then
@@ -12,10 +14,18 @@ fi
 cd $RTC_BASE || exit 1
 gclient sync
 
-echo "=========== compile debug version ==========="
+echo "=========== compile arm64 debug version ==========="
 gn gen ${ARM64_OUT} --args='target_os="android" target_cpu="arm64"'
 ninja -C ${ARM64_OUT} sdk/android || exit 1
 
-echo "=========== compile release version ==========="
+echo "=========== compile arm64 release version ==========="
 gn gen ${ARM64_RELEASE} --args='is_debug=false is_component_build=false rtc_include_tests=false target_os="android" target_cpu="arm64"'
-ninja -C ${ARM64_RELEASE} sdk/android
+ninja -C ${ARM64_RELEASE} sdk/android || exit 1
+
+echo "=========== compile arm32 debug version ==========="
+gn gen ${ARM64_OUT} --args='target_os="android" target_cpu="arm"'
+ninja -C ${ARM64_OUT} sdk/android || exit 1
+
+echo "=========== compile arm32 release version ==========="
+gn gen ${ARM64_RELEASE} --args='is_debug=false is_component_build=false rtc_include_tests=false target_os="android" target_cpu="arm"'
+ninja -C ${ARM64_RELEASE} sdk/android || exit 1
